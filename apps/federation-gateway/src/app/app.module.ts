5131c9660a69e750ff6bf8a628ec11363481045d
Paul Shpilsher <paul.shpilsher@gmail.com>
2022-12-31 15:47:31 +0300

    add auth middleware

--- apps/federation-gateway/src/app/app.module.ts
+++ apps/federation-gateway/src/app/app.module.ts
@@ -1,10 +1,30 @@
 import { IntrospectAndCompose } from '@apollo/gateway';
 import { ApolloGatewayDriverConfig, ApolloGatewayDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { RequestHandler } from '@nestjs/common/interfaces';
 import { GraphQLModule } from '@nestjs/graphql';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { verify } from 'jsonwebtoken';
+
+
+const handleAuthContext = ({ req }) => {
+  if (req && req.headers.authorization) {
+    try {
+      const jwtToken = req.headers.authorization.split('Bearer ')[1]
+      const decodedJwt = verify(jwtToken, process.env.SECRET );
+      return {user: decodedJwt};
+    }
+    catch(err) {
+      //throw Http error if required.
+      return;
+    }
+  }
+  else
+    //throw Http error if required.
+    return;
+};
 
 @Module({
   imports: [
@@ -13,6 +33,7 @@ import { AppService } from './app.service';
       server: {
         // ... Apollo server options
         cors: true,
+        context: handleAuthContext
       },
       gateway: {
         supergraphSdl: new IntrospectAndCompose({
